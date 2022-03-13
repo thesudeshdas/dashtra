4808681433f7238628029ac4cfc91b0dc7dad37d
thesudeshdas <sudeshkumardas7@gmail.com>
2022-03-13 23:43:26 +0530

    feat: basic page for navigation with other pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Homepage from './homepage/Homepage';
+import NavFooterPages from './nav-footer-pages/NavFooterPages';
+
+export { Homepage, NavFooterPages };
---
+++ src/pages/nav-footer-pages/NavFooterPages.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { NavigationTop } from '../../components';
+
+import './nav-footer-pages.css';
+
+export default function NavFooterPages() {
+  const navigate = useNavigate();
+
+  // ?Possibly a hack, maybe find a better way
+  useEffect(() => {
+    navigate('/home');
+  }, [navigate]);
+
+  return (
+    <div className='page-nav-footer-page'>
+      <NavigationTop />
+      <Outlet />
+    </div>
+  );
+}
