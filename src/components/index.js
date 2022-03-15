7b79985c2905b0313d6fd3ecbb20e3b306921572
thesudeshdas <sudeshkumardas7@gmail.com>
2022-03-16 02:54:40 +0530

    feat: basic product card

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import CardHomepageCategory from './card-homepage-category/CardHomepageCategory';
+import CardProduct from './card-product/CardProduct';
+import Filter from './filter/Filter';
 import NavigationTop from './navigation-top/NavigationTop';
 
-export { CardHomepageCategory, NavigationTop };
+export { CardHomepageCategory, CardProduct, Filter, NavigationTop };
