4d332d985d72e93a2e14735685931e371b4968d1
thesudeshdas <sudeshkumardas7@gmail.com>
2022-03-16 02:55:49 +0530

    temp: temporary files, will be changed later

---
+++ src/data/products.data.js
@@ -0,0 +1,133 @@
+export const productsData = {
+  id: 'products001',
+  productsList: [
+    {
+      id: 'p001',
+      brand: 'Nike',
+      categories: ['football', 'accessories'],
+      description: 'The best football for all tpes of grounds',
+      fastDelivery: true,
+      images: [
+        {
+          url: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ef7c6db3-c897-4ab6-b787-7b2d7ad11190/strike-football-F8tjl8.png',
+          alt: 'Nike Strike',
+        },
+      ],
+      link: 'https://www.nike.com/gb/t/strike-football-F8tjl8',
+      name: 'Nike Strike Football',
+      price: {
+        original: 3999,
+        discounted: 2999,
+        discount: 10,
+      },
+      rating: {
+        stars: 5,
+        number: 67,
+      },
+      specifications: [
+        {
+          heading: 'Color',
+          detail: 'White/Red',
+        },
+      ],
+      status: 'active',
+      stock: 20,
+    },
+    {
+      id: 'p002',
+      brand: 'Nike',
+      categories: ['football', 'accessories'],
+      description: 'The best football for all tpes of grounds',
+      fastDelivery: true,
+      images: [
+        {
+          url: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ef7c6db3-c897-4ab6-b787-7b2d7ad11190/strike-football-F8tjl8.png',
+          alt: 'Nike Strike',
+        },
+      ],
+      link: 'https://www.nike.com/gb/t/strike-football-F8tjl8',
+      name: 'Nike Strike Football',
+      price: {
+        original: 3999,
+        discounted: 2999,
+        discount: 10,
+      },
+      rating: {
+        stars: 5,
+        number: 67,
+      },
+      specifications: [
+        {
+          heading: 'Color',
+          detail: 'White/Red',
+        },
+      ],
+      status: 'active',
+      stock: 20,
+    },
+    {
+      id: 'p003',
+      brand: 'Nike',
+      categories: ['football', 'accessories'],
+      description: 'The best football for all tpes of grounds',
+      fastDelivery: true,
+      images: [
+        {
+          url: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ef7c6db3-c897-4ab6-b787-7b2d7ad11190/strike-football-F8tjl8.png',
+          alt: 'Nike Strike',
+        },
+      ],
+      link: 'https://www.nike.com/gb/t/strike-football-F8tjl8',
+      name: 'Nike Strike Football',
+      price: {
+        original: 3999,
+        discounted: 2999,
+        discount: 10,
+      },
+      rating: {
+        stars: 5,
+        number: 67,
+      },
+      specifications: [
+        {
+          heading: 'Color',
+          detail: 'White/Red',
+        },
+      ],
+      status: 'active',
+      stock: 20,
+    },
+    {
+      id: 'p004',
+      brand: 'Nike',
+      categories: ['football', 'accessories'],
+      description: 'The best football for all tpes of grounds',
+      fastDelivery: true,
+      images: [
+        {
+          url: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ef7c6db3-c897-4ab6-b787-7b2d7ad11190/strike-football-F8tjl8.png',
+          alt: 'Nike Strike',
+        },
+      ],
+      link: 'https://www.nike.com/gb/t/strike-football-F8tjl8',
+      name: 'Nike Strike Football',
+      price: {
+        original: 3999,
+        discounted: 2999,
+        discount: 10,
+      },
+      rating: {
+        stars: 5,
+        number: 67,
+      },
+      specifications: [
+        {
+          heading: 'Color',
+          detail: 'White/Red',
+        },
+      ],
+      status: 'active',
+      stock: 20,
+    },
+  ],
+};
