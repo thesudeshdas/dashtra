b72c1ce36e8e8be29740993e04f5845ef25e4aca
thesudeshdas <sudeshkumardas7@gmail.com>
2022-04-17 00:53:23 +0530

    refactor: UI changes & class addition

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -50,6 +50,17 @@ export function MajesticonsTruckLine(props) {
   );
 }
 
+export function MiClose(props) {
+  return (
+    <svg width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+      <path
+        fill='currentColor'
+        d='M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12L5.293 6.707a1 1 0 0 1 0-1.414z'
+      ></path>
+    </svg>
+  );
+}
+
 export function MiHeart(props) {
   return (
     <svg width='1em' height='1em' viewBox='0 0 24 24' {...props}>
