6636bf2336b275f81151503a73fdc1bd1aaa47a2
thesudeshdas <sudeshkumardas7@gmail.com>
2022-05-28 00:40:43 +0530

    feat: wishlist card made

---
+++ src/components/card-product-wishlist/CardProductWishlist.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { MiShoppingCart } from '../../assets/icons';
+import './CardProductWishlist.css';
+
+export default function CardProductWishlist({ details }) {
+  const { _id, brand, images, name, price } = details;
+
+  return (
+    <div className='card-product--wishlist padding-md'>
+      <Link to={`/product/${_id}`}>
+        <img src={images[0].src} alt='Nike Strike' className='image' />
+        <h4 className='font-weight-600'>{brand}</h4>
+        <h3 className='font-size-ms font-weight-400'>{name}</h3>
+        <p className='font-size-sm'>
+          <strong className='font-size-ms'>₹ {price.discounted} </strong>
+          {price.discounted !== price.original && (
+            <>
+              <span className='text-strike margin-horizontal-xs'>
+                ₹ {price.original}
+              </span>{' '}
+              <span>{price.discount}%</span>
+            </>
+          )}
+        </p>
+      </Link>
+
+      <div className='card-product__container--wishlist grid-center'>
+        <button
+          className='button button-outline-secondary flex-center'
+          onClick={() => console.log('add to wishlist')}
+        >
+          <MiShoppingCart className='margin-right-xs font-size-ml' />
+          MOVE TO CART
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import CardHomepageCategory from './card-homepage-category/CardHomepageCategory';
 import CardProduct from './card-product/CardProduct';
 import CardProductCart from './card-product-cart/CardProductCart';
+import CardProductWishlist from './card-product-wishlist/CardProductWishlist';
 import Filter from './filter/Filter';
 import NavigationTop from './navigation-top/NavigationTop';
 
@@ -8,6 +9,7 @@ export {
   CardHomepageCategory,
   CardProduct,
   CardProductCart,
+  CardProductWishlist,
   Filter,
   NavigationTop,
 };
