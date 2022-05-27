382d2084c2aabbcbae71009b1970af160065615c
thesudeshdas <sudeshkumardas7@gmail.com>
2022-05-28 00:54:27 +0530

    feat: initial load for wishlist

--- src/contexts/wishlist.context.js
+++ src/contexts/wishlist.context.js
@@ -1,16 +1,65 @@
-import { createContext, useContext } from 'react';
+import axios from 'axios';
+import { createContext, useContext, useEffect, useReducer } from 'react';
+import { useAuth } from './auth.context';
 
 const WishlistContext = createContext();
 
 export default function WishlistProvider({ children }) {
+  const {
+    state: { signInStatus, userId },
+  } = useAuth();
+
+  const initialState = {
+    owner: '',
+    wishlist: [],
+  };
+
+  const wishlistReducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_INITIAL_DATA_FROM_SERVER':
+        return {
+          ...state,
+          owner: action.payload.wishlist.owner,
+          wishlist: action.payload.wishlist.productsList,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(wishlistReducer, initialState);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (userId) {
+          const response = await axios.get(
+            `http://localhost:3000/wishlist/${userId}`
+          );
+
+          console.log({ response });
+
+          if (response.status === 200) {
+            dispatch({
+              type: 'FETCH_INITIAL_DATA_FROM_SERVER',
+              payload: response.data,
+            });
+          }
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    })();
+  }, [signInStatus, userId]);
+
   return (
-    <WishlistContext.Provider value={{ test: 'success for wish' }}>
+    <WishlistContext.Provider value={{ state, dispatch }}>
       {children}
     </WishlistContext.Provider>
   );
 }
 
-
 export function useWishlist() {
-  return useContext(WishlistContext)
+  return useContext(WishlistContext);
 }
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,18 +1,18 @@
 import { CardProductWishlist } from '../../components';
-import { useCart } from '../../contexts/cart.context';
+import { useWishlist } from '../../contexts/wishlist.context';
 import './Wishlist.css';
 
 export default function Wishlist() {
   const {
-    state: { cartList, total },
-  } = useCart();
+    state: { wishlist },
+  } = useWishlist();
 
   return (
     <main className='page-wishlist'>
       <section className='padding-md'>
         <h2 className='margin-bottom-md'>My Wishlist</h2>
         <ul className='products-container--wishlist'>
-          {cartList.map((product) => (
+          {wishlist.map((product) => (
             <li key={product._id} className='margin-bottom-sm'>
               <CardProductWishlist details={product.product} />
             </li>
