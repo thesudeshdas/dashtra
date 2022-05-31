ff4e92f5ade670490bd3dee27c6b11648c8d62c0
thesudeshdas <sudeshkumardas7@gmail.com>
2022-06-01 00:10:02 +0530

    fix: sorting logic

--- src/utils/sort.utils.js
+++ src/utils/sort.utils.js
@@ -1,7 +1,7 @@
 export const sortProducts = (products, sortBy) => {
   switch (sortBy) {
     case 'relevance':
-      return products.sort((a, b) => products.indexOf(b) - products.indexOf(a));
+      return products.sort((a, b) => products.indexOf(a) - products.indexOf(b));
 
     case 'price-low-to-high':
       return products.sort((a, b) => a.price.discounted - b.price.discounted);
